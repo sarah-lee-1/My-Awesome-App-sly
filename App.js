059b63063bc7dc9e7f930f26c9d4dfeb4678d61e
sarah-lee-1 <93781113+sarah-lee-1@users.noreply.github.com>
2022-01-31 13:17:24 -0600

    feat: Created a simple Node "Hello World" app

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 const express = require('express')
-const app = expressd()
-
+const app = express()
+// Simple Hello Word Node App
 const APP_PORT = process.env.APP_PORT || 3000; app.get('/', (req, res) => {
-    res.send('Hello World')
-}) app.listen(APP_PORT, () => {
-    console.log('Running app at port:${APP_PORT}')
+    res.send('Hello World!')
+}); app.listen(APP_PORT, () => {
+    console.log(`Running app at port:${APP_PORT}`)
 })
