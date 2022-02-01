8b048f5934af175f08734915eeb7bbc80056b043
sarah-lee-1 <93781113+sarah-lee-1@users.noreply.github.com>
2022-02-01 11:22:40 -0600

    style: Merged backward

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 // Simple Hello Word Node App
 const APP_PORT = process.env.APP_PORT || 3000; app.get('/', (req, res) => {
-    res.send('Hello Sarah! Nice to see you!')
+    res.send('Hello Sarah and the rest of the world')
 }); app.listen(APP_PORT, () => {
     console.log(`Running app at port:${APP_PORT}`)
 })
