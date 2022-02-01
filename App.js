932659f6ff0da23007a36f5216e0f1b8e2ec5368
sarah-lee-1 <93781113+sarah-lee-1@users.noreply.github.com>
2022-02-01 11:09:03 -0600

    git tag -a v0.3.1 -m "Revise msg per instructions"

--- App.js
+++ App.js
@@ -1,9 +1,8 @@
-// "Hello to Sarah and the rest of the world out there!"
 const express = require('express')
 const app = express()
 // Simple Hello Word Node App
 const APP_PORT = process.env.APP_PORT || 3000; app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send("Hello to Sarah and the rest of the world out there!")
 }); app.listen(APP_PORT, () => {
     console.log(`Running app at port:${APP_PORT}`)
 })
