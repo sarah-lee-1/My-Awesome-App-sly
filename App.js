a068672cad3175c7b2de56556783f6689d9ea0c4
sarah-lee-1 <93781113+sarah-lee-1@users.noreply.github.com>
2022-02-01 11:03:07 -0600

    git

--- App.js
+++ App.js
@@ -1,9 +1,8 @@
-// "Hello Sarah! Nice to see you" 
 const express = require('express')
 const app = express()
 // Simple Hello Word Node App
 const APP_PORT = process.env.APP_PORT || 3000; app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello Sarah! Nice to see you!')
 }); app.listen(APP_PORT, () => {
     console.log(`Running app at port:${APP_PORT}`)
 })
